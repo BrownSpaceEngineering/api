514abc8fcb52d0ac35d452f5d5eb912f9c0da5bf
RyanHecht <ryanhecht.tse@gmail.com>
2016-11-20 14:05:12 -0500

    started scaffolding for data routes

--- server/routes/data/index.js
+++ server/routes/data/index.js
@@ -1,5 +1,23 @@
 var router = require('express').Router();
 
+// Returns all data in database
+router.get('/', function(req, res, next) {
+  // Using Sequelize findAll, query database for all data
+})
+
+router.use('/temperature', require('./temperature'));
+
+router.get('/:cat/timecode/:time')
+
+//TODO: query parsing for multiple times
+/*router.get('/:cat/range/:time1', function(req, res, next) {
+//get time2 param from req,
+
+})*/
+
+//router.get('/:cat')
+
+
 
 
 module.exports = router;
---
+++ server/routes/data/temperature/index.js
@@ -0,0 +1,10 @@
+var router = require('express').Router();
+
+// Returns all temperature data in database
+router.get('/', function(req, res, next) {
+  // Using Sequelize findAll, query database for all temperature data
+})
+
+
+
+module.exports = router;
