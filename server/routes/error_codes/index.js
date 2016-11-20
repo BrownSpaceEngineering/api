2a61a87215cb454f39a157c17b93c9951c3f6e70
Iamjaredlee <jared_lee@brown.edu>
2016-11-20 14:00:11 -0500

    Filled out scaffolding for error routes

--- server/routes/error_codes/index.js
+++ server/routes/error_codes/index.js
@@ -5,11 +5,19 @@ router.get('/', function (req, res, next) {
   // Using Sequelize, query the database in error tables for all error codes and return with status 100
 })
 
+// Return all instances where error code 'id' has occured
+router.get('/:id', function (req, res, next) {
+	// Using Sequelize, query the database in error tables for error code = 'id'
+})
 
-// router.get('/:id')
+// Write a new error code to the database 
+router.post('/', function (req, res , next ) {
+	// Using Sequelize, write a new error to the database
+})
 
-// router.post('/')
-
-// router.put('/:id')
+// Update the database entry for a given error code
+router.put('/:id', function (req, res, next) {
+	// Using Sequelize, update the database in error tables where error code = 'id'
+})
 
 module.exports = router;
