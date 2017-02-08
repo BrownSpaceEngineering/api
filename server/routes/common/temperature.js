f612c4768175b2b4010f425cff493e9cc0a4a611
RyanHecht <ryanhecht.tse@gmail.com>
2017-02-08 15:24:16 -0500

    Generalized temperature common functions

--- server/routes/common/temperature.js
+++ server/routes/common/temperature.js
@@ -1,38 +1,36 @@
 var db = require('../../database/_db');
 
-var Temperature = db.model('temperature');
+//var Temperature = db.model('temperature');
 
 var exportObj = {};
 
 exportObj = {};
 
 // get all
-exportObj.findAllTemperatures = function(){
-  return Temperature.findAll();
+exportObj.findAllTemperatures = function(model){
+
+  return db.model(model).findAll();
 };
 
 // get one
-exportObj.getTemperatureByID = function(id){
-  return Temperature.findOne({ where: { id:id } });
+exportObj.getTemperatureByID = function(model, id){
+  return db.model(model).findOne({ where: { id:id } });
 };
 
 // add one
-exportObj.addTemperature = function(temp){
-  return Temperature.create(temp)
+exportObj.addTemperature = function(model, temp){
+  return db.model(model).create(temp)
   .then(function(createdTemp){
-    Temperature.findOrCreate(createdTemp); 
+    db.model(model).findOrCreate(createdTemp);
   });
 };
 
 // update one (Someone please fix this cause I have no idea what I'm doing)
-exportObj.updateTemperature = function(id, newTemp){
-  return Temperature.update(newTemp, { where: { id: id } });
+exportObj.updateTemperature = function(model, id, newTemp){
+  return db.model(model).update(newTemp, { where: { id: id } });
 };
 
 // delete a temperature reading based on id
-exportObj.deleteTemperature = function(id){
-  return Temperature.destroy({ where: { id: id } });
+exportObj.deleteTemperature = function(model, id){
+  return db.model(model).destroy({ where: { id: id } });
 };
-
-
- 
