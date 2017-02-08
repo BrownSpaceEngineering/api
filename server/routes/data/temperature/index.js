128ecdf76d0cb2d8fe533ff29a5d28af7b07641a
RyanHecht <ryanhecht.tse@gmail.com>
2017-02-08 15:27:40 -0500

    Strucured data folder to group by component. Deprecated data/temperature.js, keeping for posterity

--- server/routes/data/temperature/index.js
+++ server/routes/data/temperature/index.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-
+//DEPRECATED - WE SHOULD BREAK IT UP BY COMPONENT INSTEAD
 // Returns all temperature data in database
 router.get('/', function(req, res, next) {
   // Using Sequelize findAll, query database for all temperature data
