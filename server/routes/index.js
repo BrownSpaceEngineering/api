c8e2b0bd09d1a6ca205a787535498b8f31390469
Ryan Hecht <ryan_hecht@brown.edu>
2017-09-23 13:09:20 -0400

    Added test route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,7 @@ var router = require('express').Router();
 
 router.use('/receive_data', require('./receiveData'));
 router.use('/data', require('./data'));
-
+router.get('/', function (req, res, next){
+  res.send("It works!");
+});
 module.exports = router;
