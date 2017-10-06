5635965f8a821e495c248b41b797aa4462843aaa
Ali Mir <ali.ahmed.mir@gmail.com>
2017-10-06 14:24:26 -0400

    attitude IMU Accelerometer first GET

---
+++ server/routes/data/dataAttitudes/attitudeImuAccelerometer.js
@@ -0,0 +1,24 @@
+var router = require('express').Router();
+var AttitudeImuAccelerometer = require('../common/dataAttitude/attitudeImuAccelerometer');
+
+router.get('/', function (req, res, next){
+  AttitudeImuAccelerometer.findAllAttitudeImuAccelerometer()
+  .then(function (attitudeImuAccelerometer){
+    res.send(attitudeImuAccelerometer);
+  })
+  .catch(function (err){
+    console.error(err);
+    res.status(500).send(err);
+  });
+});
+
+router.get('/:id', function (req, res, next){
+  AttitudeImuAccelerometer.getAttitudeImuAccelerometerById(req.params.id)
+  .then(function (attitudeImuAccelerometer){
+    res.send(attitudeImuAccelerometer);
+  })
+  .catch(function (err){
+    console.error(err);
+    res.status(500).send(err);
+  });
+});
