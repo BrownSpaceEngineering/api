01b84ef51c1c49a4f179d3d407a1369f997e49d5
Ryan Hecht <ryanhecht.tse@gmail.com>
2017-10-15 13:14:45 -0400

    Added dbConfig.js to avoid having login information in source code

---
+++ dbConfig.js
@@ -0,0 +1,15 @@
+dialect = "postgres"
+host = "localhost"
+port = "5432"
+username = "user"
+password = "password"
+database = "databaseName"
+
+module.exports = {
+  dialect: dialect,
+  host: host,
+  port: port,
+  username: username,
+  password: password,
+  database: database
+}
