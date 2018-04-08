0d2b0adadf83af2aae2876a130748102081adc7a
Ryan Hecht <ryanhecht.tse@gmail.com>
2018-04-08 15:12:24 -0400

    Added username/password to env template

--- server/database/_db.js
+++ server/database/_db.js
@@ -1,11 +1,11 @@
 var Sequelize = require('sequelize');
 var url = process.env.DB_URL;
-
-// var config = require('../../dbConfig.js');
-// url = config.dialect + "://" + config.username + ":" + config.password + "@" + config.host + ":" + config.port + "/" + config.database;
-
+var user = process.env.DB_USER;
+var pw = process.env.DB_PASSWORD;
 
 module.exports = new Sequelize(url, {
+  username: user,
+  password: pw,
   logging: false,
   native: true
 })
